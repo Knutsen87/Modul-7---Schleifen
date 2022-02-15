4bb30de52a0b17c78c3d15f1667b1197e8b857b7
Dennis Gillich <dennisgillich@live.de>
2022-02-15 18:18:33 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] names = new string[]
+{
+    "Sebastian",
+    "Sabrina",
+    "Horst",
+    "Claudia",
+    "Kim",
+    "Marci"
+};
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+    Console.WriteLine("\n");
+}
+
+
+string[,] products = new string[,]
+{
+    {
+        "Apfel",
+        "Smartphone",
+        "Schokolade"
+    },
+    {
+        "Lebensmittel",
+        "Elektronik",
+        "Lebensmittel"
+    }
+};
+
+for(int zeile = 0; zeile < products.GetLength(1); zeile++)
+{
+    for (int spalte = 0; spalte < products.GetLength(0); spalte++)
+    {
+        Console.WriteLine(products[spalte, zeile]);
+        Console.Write(" ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.ReadKey();
